856e46ae87c034c7132181bc0af666be31240942
pointoboom <59865507+pointoboom@users.noreply.github.com>
2023-06-21 23:18:47 +0700

    change name server

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,7 +2,7 @@ import { Flex, Spacer } from '@chakra-ui/react'
 import Createpost from '../components/Createpost';
 import Dashboard from '../components/Dashboard';
 import socketIO from "socket.io-client";
-const socket = socketIO.connect("http://13.229.133.88:4000");
+const socket = socketIO.connect("http://backend:4000");
 function Homepage() {
     return (
         <>
